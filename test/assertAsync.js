6dcb066b3264ada904d775acef02e788dfe3143d
François Parmentier <francois.parmentier@gmail.com>
2015-02-15 13:31:45 +0100

    Add assert asynchronous tests
    Warning: some are not working yet:
    1, 3, 5bis, and 6bis.

--- test/assertAsync.js
+++ test/assertAsync.js
@@ -4,7 +4,7 @@
 var assert = require('assert')
   , JBJ = require('..');
 
-describe('breakif', function () {
+describe('asynchronous breakif', function () {
 
   var input = {
     "val" : "unmodified",
@@ -12,52 +12,60 @@ describe('breakif', function () {
     "b" : "one"
   };
 
-  it('#1', function() {
+  it('#1', function(done) {
     var stylesheet = {
       "$val" : {
         "breakif": "a > 0",
         "set" : "modified"
       }
     };
-    var output = JBJ.renderSync(stylesheet, input);
-    assert.equal(output.val, null);
+    var output = JBJ.render(stylesheet, input, function (err, output) {
+      assert.equal(output.val, null);
+      done(err);
+    });
   });
 
-  it('#2', function() {
+  it('#2', function(done) {
     var stylesheet = {
       "$val" : {
         "breakIf": "a < 0",
         "set" : "modified"
       }
     };
-    var output = JBJ.renderSync(stylesheet, input);
-    assert.equal(output.val, "modified");
+    var output = JBJ.render(stylesheet, input, function (err, output) {
+      assert.equal(output.val, "modified");
+      done(err);
+    });
   });
 
-  it('#3', function() {
+  it('#3', function(done) {
     var stylesheet = {
       "$val" : {
         "breakIf": "b == \"one\"",
         "set" : "modified"
       }
     };
-    var output = JBJ.renderSync(stylesheet, input);
-    assert.equal(output.val, null);
+    var output = JBJ.render(stylesheet, input, function (err, output) {
+      assert.equal(output.val, null);
+      done(err);
+    });
   });
 
-  it('#4', function() {
+  it('#4', function(done) {
     var stylesheet = {
       "$val" : {
         "breakIf": "b != \"one\"",
         "set" : "modified"
       }
     };
-    var output = JBJ.renderSync(stylesheet, input);
-    assert.equal(output.val, "modified");
+    var output = JBJ.render(stylesheet, input, function (err, output) {
+      assert.equal(output.val, "modified");
+      done(err);
+    });
   });
 
 
-  it('#5', function() {
+  it('#5', function(done) {
     var stylesheet = {
       "$val#1" : {
         "breakIf": "a == 1",
@@ -67,13 +75,14 @@ describe('breakif', function () {
         "breakIf": "a != 1",
         "set" : "else"
       }
-
     };
-    var output = JBJ.renderSync(stylesheet, input);
-    assert.equal(output.val, "else");
+    var output = JBJ.render(stylesheet, input, function (err, output) {
+      assert.equal(output.val, "else");
+      done(err);
+    });
   });
 
-  it('#5 bis', function() {
+  it('#5 bis', function(done) {
     var stylesheet = {
       "$val#1" : {
         "breakIf": "a != 1",
@@ -83,13 +92,14 @@ describe('breakif', function () {
         "breakIf": "a == 1",
         "set" : "else"
       }
-
     };
-    var output = JBJ.renderSync(stylesheet, input);
-    assert.equal(output.val, null);
+    var output = JBJ.render(stylesheet, input, function (err, output) {
+      assert.equal(output.val, null);
+      done(err);
+    });
   });
 
-  it('#5 ter', function() {
+  it('#5 ter', function(done) {
     var stylesheet = {
       "$val#1" : {
         "set" : "if"
@@ -98,13 +108,14 @@ describe('breakif', function () {
         "breakIf": "a != 1",
         "set" : "else"
       }
-
     };
-    var output = JBJ.renderSync(stylesheet, input);
-    assert.equal(output.val, "else");
+    var output = JBJ.render(stylesheet, input, function (err, output) {
+      assert.equal(output.val, "else");
+      done(err);
+    });
   });
 
-  it('#6', function() {
+  it('#6', function(done) {
     var stylesheet = {
       "$val#1" : {
         "assert": "a == 1",
@@ -114,13 +125,14 @@ describe('breakif', function () {
         "get" : "val",
         "default": "else val",
       }
-
     };
-    var output = JBJ.renderSync(stylesheet, input);
-    assert.equal(output.val, "if val");
+    var output = JBJ.render(stylesheet, input, function (err, output) {
+      assert.equal(output.val, "if val");
+      done(err);
+    });
   });
 
-  it('#6 bis', function() {
+  it('#6 bis', function(done) {
     var stylesheet = {
       "$val#1" : {
         "assert": "a != 1",
@@ -130,12 +142,14 @@ describe('breakif', function () {
         "get" : "val",
         "default": "else val",
       }
-   };
-    var output = JBJ.renderSync(stylesheet, input);
-    assert.equal(output.val, "else val");
+    };
+    var output = JBJ.render(stylesheet, input, function (err, input) {
+      assert.equal(output.val, "else val");
+      done(err);
+    });
   });
 
-  it('#7', function() {
+  it('#7', function(done) {
     var stylesheet = {
       "$val" : {
         "get" : "a",
@@ -143,8 +157,10 @@ describe('breakif', function () {
         "set" : "if val"
       }
     };
-    var output = JBJ.renderSync(stylesheet, input);
-    assert.equal(output.val, "if val");
+    var output = JBJ.render(stylesheet, input, function (err, output) {
+      assert.equal(output.val, "if val");
+      done(err);
+    });
   });
 
 });
