95f2f308e447818fe3e238d5b990bc0efca1fa08
François Parmentier <francois.parmentier@gmail.com>
2016-03-16 09:47:08 +0100

    :rotating_light: Add sortBy test

--- test/test.js
+++ test/test.js
@@ -3,7 +3,9 @@
 'use strict';
 var assert = require('assert')
   , JBJ = require('../')
-  , examples = require('./slug.json');
+  , examples = require('./slug.json')
+  , examplesEjs = require('./ejs.json')
+  ;
 
 describe('slug', function () {
   Object.keys(examples).forEach(function (example) {
@@ -11,6 +13,20 @@ describe('slug', function () {
       var input      = examples[example].input;
       var stylesheet = examples[example].stylesheet;
       var expected   = examples[example].expected;
+      JBJ.render(stylesheet, input, function (err, output) {
+        assert.deepEqual(output, expected);
+        done(err);
+      });
+    });
+  });
+});
+
+describe('ejs', function () {
+  Object.keys(examplesEjs).forEach(function (example) {
+    it(example, function (done) {
+      var input      = examplesEjs[example].input;
+      var stylesheet = examplesEjs[example].stylesheet;
+      var expected   = examplesEjs[example].expected;
       JBJ.render(stylesheet, input, function (err, output) {
         assert.deepEqual(output, expected);
         done(err);
